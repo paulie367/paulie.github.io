601d4cb804a6b30bf859dbb9f37d21cf04bcc078
chananegan <63941327+chananegan@users.noreply.github.com>
2020-11-11 10:16:06 +0900

    Create list.py

---
+++ web/syntax/list.py
@@ -0,0 +1,11 @@
+s = [1, 'four', 9, 16, 25] #list and elements
+print(s)
+print(s[-1]) #index
+print(s[-2])
+print(len(s)) #list length
+s[1] = 4
+print(s)
+del s[2]
+print(s)
+s.append('paul')
+print(s)
