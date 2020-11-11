1fc3bc1a202d9b62ca97854994990201a489f7f7
chananegan <63941327+chananegan@users.noreply.github.com>
2020-11-11 10:15:24 +0900

    Create boolean.py

---
+++ web/syntax/boolean.py
@@ -0,0 +1,19 @@
+#integer
+print(1)
+#string
+print("hello world")
+#boolean
+print(True)
+print(False)
+#Expression
+print(1+1)
+print('hello ' + 'world') 
+#comparison operator
+print(1==1)
+print(1<2)
+print(1>2)
+#membership operator
+print("bucks" in "starbucks")
+#check file exists in directory
+import os.path
+print(os.path.exists('boolean.py')) #in syntax directory
